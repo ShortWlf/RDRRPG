0ca83fcbd420c6b07b03acd2286fb09b6712cd15
bliffy <invaderjolleyolleyman.busines@gmail.com>
2020-06-06 08:24:00 -0400

    introduce g_* global vars files

---
+++ resources/RedM-info-master/mytest.lua
@@ -0,0 +1,16 @@
+
+G_COWS = {
+  BIG = {
+   HEFER = "yellow",
+   BRIMMER = "brown",
+  },
+  SMALL = {
+   HEFER = "red",
+   POOY = "green",
+  },
+};
+
+print("Hello World")
+print(G_COWS.BIG.HEFER)
+print(G_COWS.SMALL.POOY)
+
